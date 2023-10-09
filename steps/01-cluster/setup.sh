433f110ed8ad92e3eb1e7261f303dd7f331bd127
jediminer543 <jediminer543@users.noreply.github.com>
2023-10-10 00:03:15 +0100

    More work on cluster setup

---
+++ steps/01-cluster/setup.sh
@@ -0,0 +1,25 @@
+# Install containerd
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install containerd.io
+sudo systemctl enable containerd
+# Configure containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+PAUSE_IMAGE=$(kubeadm config images list | grep pause)
+sudo -E sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml
+sudo systemctl restart containerd
+# Install kubeadm
+
+# Configure kubeadm
+sudo kubeadm init --config config/kubeadm-config.yaml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# Configure Calico
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+kubectl create -f config/calico-config.yaml
+# Configure local storage
+kubectl apply -f config/local-path-storage.yaml
---
+++ steps/02-storage/setup.sh
@@ -0,0 +1,3 @@
+kubectl create -f https://raw.githubusercontent.com/rook/rook/v1.12.5/deploy/examples/crds.yaml
+kubectl create -f https://raw.githubusercontent.com/rook/rook/v1.12.5/deploy/examples/common.yaml
+kubectl create -f https://raw.githubusercontent.com/rook/rook/v1.12.5/deploy/examples/operator.yaml
