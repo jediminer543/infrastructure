bb777555ff8582ef2ca99616260dbcdaf2c5f09a
jediminer543 <jediminer543@users.noreply.github.com>
2023-10-29 22:53:52 +0000

    Fix issue with versioning?

--- tf/lhs_monitoring_deploy/main.tf
+++ tf/lhs_monitoring_deploy/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "2.6.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "2.11.0"
     }
   }
 }
