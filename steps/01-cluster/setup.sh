ed6986ce6ba39c98ea724bfa15d26e2ecf7144ee
jediminer543 <jediminer543@users.noreply.github.com>
2024-03-30 19:38:48 +0000

    actually install kubeadm

--- steps/01-cluster/setup.sh
+++ steps/01-cluster/setup.sh
@@ -1,3 +1,6 @@
+# Install deps
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gpg
 # Install containerd
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
@@ -12,7 +15,11 @@ PAUSE_IMAGE=$(kubeadm config images list | grep pause)
 sudo -E sed -i "s,sandbox_image = .*,sandbox_image = \"$PAUSE_IMAGE\",g" /etc/containerd/config.toml
 sudo systemctl restart containerd
 # Install kubeadm
-
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
 # Configure kubeadm
 sudo kubeadm init --config config/kubeadm-config.yaml
 mkdir -p $HOME/.kube
