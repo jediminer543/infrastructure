a5050582ee8a82b311450863440b3f879641409c
jediminer543 <jediminer543@users.noreply.github.com>
2024-03-30 19:31:34 +0000

    debianise

--- steps/01-cluster/setup.sh
+++ steps/01-cluster/setup.sh
@@ -1,7 +1,7 @@
 # Install containerd
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/debian.list > /dev/null
 sudo apt update
 sudo apt install containerd.io
 sudo systemctl enable containerd
