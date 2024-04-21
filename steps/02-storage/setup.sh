8d366cc5d5d9db5d7966d7cfdcfea7173d0ed977
jediminer543 <jediminer543@users.noreply.github.com>
2024-04-21 23:28:53 +0100

    refactor and fix storage class

--- steps/02-storage/setup.sh
+++ steps/02-storage/setup.sh
@@ -2,4 +2,4 @@
 # kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.5.2/deploy/prerequisite/longhorn-nfs-installation.yaml
 # kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.5.2/deploy/longhorn.yaml
 
-kubectl apply -f config
+kubectl apply -f config/
