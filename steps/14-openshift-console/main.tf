579d425663a3bea9b47afcbc8005afaec837d76f
jediminer543 <jediminer543@users.noreply.github.com>
2024-08-17 11:32:01 +0100

    add more work

---
+++ steps/14-openshift-console/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    authentik = {
+      source = "goauthentik/authentik"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = var.kubectl_config
+}
+
+
--- steps/21-hedgedoc/main.tf
+++ steps/21-hedgedoc/main.tf
@@ -12,3 +12,22 @@ terraform {
 provider "kubernetes" {
   config_path = var.kubectl_config
 }
+
+data "terraform_remote_state" "authentication" {
+    backend = "local"
+
+    config = {
+        path = "../11-authentication/terraform.tfstate"
+    }
+}
+
+provider "authentik" {
+  url   = "https://${data.terraform_remote_state.authentication.outputs.authentik_fqdn}"
+  token = data.terraform_remote_state.authentication.outputs.authentik_key
+}
+
+module "hedgedoc" {
+  source = "../../tf/hedgedoc_deploy"
+  authentik_fqdn = "authentik.dev.gwen.org.uk"
+  hedgedoc_fqdn = "hedgedoc.dev.gwen.org.uk"
+}
