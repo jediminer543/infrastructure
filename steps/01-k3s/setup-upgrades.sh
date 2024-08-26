c872e260bc4f500e34f173159f76829094fd3ddd
jediminer543 <jediminer543@users.noreply.github.com>
2024-08-26 17:27:53 +0100

    add upgrade setup logic

---
+++ steps/01-k3s/setup-upgrades.sh
@@ -0,0 +1,3 @@
+# from: https://docs.k3s.io/upgrades/automated
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/crd.yaml
